2c4f90ae8f8d27f20fd9b14abcbc42a77d1ef71d
salih kocamaz <slh-38@hotmail.com>
2022-07-02 18:13:40 +0300

    Add project files.

---
+++ Blazor.DataAccess/Data/BlazorContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.DataAccess.Data
+{
+    public class BlazorContext:DbContext
+    {
+        public BlazorContext(DbContextOptions<BlazorContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Blazor.DataAccess/Migrations/20220702150415_firsttt.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blazor.DataAccess.Migrations
+{
+    public partial class firsttt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
